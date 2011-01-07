b6fe3ff3b8290e2eb8c547a8a219ac1cfc3bd561
daKmoR <daKmoR@gmx.at>
2011-01-07 16:30:01 +0100

    FlexSlide.Setting.Tab, FlexSlide.Setting.TabVertical

---
+++ Settings/Galleries/FlexSlide/Tab/FlexSlide.Setting.Tab.js
@@ -0,0 +1,30 @@
+/*
+---
+
+name: FlexSlide.Setting.Tab
+
+description: a TabVertical style for FlexSlide
+
+license: MIT-style license.
+
+requires: [FlexSlide]
+
+provides: [FlexSlide.Setting.Tab]
+
+...
+*/
+
+SettingsStore.FlexSlide = SettingsStore.FlexSlide || {};
+
+SettingsStore.FlexSlide.Tab = {
+	selections: {
+		'item': '.tab',
+		'select': '.tabTitle'
+	},
+	auto: false,
+	getSizeFromContainer: false,
+	autoContainerSize: { x: false, y: true },
+	centerItem: false,
+	render: ['select', 'item'],
+	effect: { up: 'slideLeftQuart', down: 'slideRightQuart' }
+};
