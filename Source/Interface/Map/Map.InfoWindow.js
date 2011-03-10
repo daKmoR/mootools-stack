8b1ad7359efd45208b9d9c1d93e60d0b2403b8bb
daKmoR <daKmoR@gmx.at>
2011-03-10 10:46:10 +0100

    ajax/request support for InfoWindow

--- Source/Interface/Map/Map.InfoWindow.js
+++ Source/Interface/Map/Map.InfoWindow.js
@@ -24,11 +24,15 @@ Map.InfoWindow = new Class({
 	options: {
 		// use all options from http://code.google.com/apis/maps/documentation/javascript/reference.html#InfoWindowOptions
 		content: 'pls provide your content with the options',
+		url: '',
+		onSuccess: function(responseTree, responseElements, responseHTML, responseJavaScript) {
+			this.setContent(responseHTML);
+		}.bind(this)
 	},
 	
 	subObjectMapping: {
 		'this.infoWindowObj': {
-			functions: ['close', 'getPosition', 'open', 'setOptions'],
+			functions: ['close', 'getPosition', 'setOptions'],
 			properties: ['content', 'zIndex'],
 			eventInstance: 'google.maps.event',
 			eventAddFunction: 'addListener',
@@ -54,6 +58,19 @@ Map.InfoWindow = new Class({
 	setPosition: function(point) {
 		var point = typeOf(point) === 'array' ? point.toLatLng() : point;
 		this.infoWindowObj.setPosition(point);
+	},
+	
+	// MVC object is usually a marker.
+	open: function(map, MVCObject) {
+		this.infoWindowObj.open(map, MVCObject);
+		if (this.options.url !== '') {
+			this.getRequest({url: this.options.url}).send();
+		}
+	},
+	
+	getRequest: function(options) {
+		var options = Object.merge(this.options, options);
+		return this.request ? this.request.setOptions(options) : this.request = new Request.HTML(options);
 	}
 
 });
