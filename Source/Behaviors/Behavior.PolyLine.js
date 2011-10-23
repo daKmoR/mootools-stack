3ec10ef8983553d6d4fea8bc10aa44493194e141
daKmoR <thomas.allmer@webteam.at>
2011-10-23 17:14:55 +0200

    polyline options

--- Source/Behaviors/Behavior.PolyLine.js
+++ Source/Behaviors/Behavior.PolyLine.js
@@ -13,15 +13,19 @@ Behavior.addGlobalFilter('PolyLine', {
 
 	defaults: {
 		target: '!div [data-behavior="Map"]',
-		animated: true,
-		color: '#000'
-	},	
+		animated: false,
+		color: '#000',
+		opacity: 0.7,
+		weight: 2
+	},
 
 	setup: function(element, api) {
 		var animated = api.getAs(Boolean, 'animated'),
 			map = element.getElement(api.getAs(String, 'target')).getBehaviorResult('Map'),
 			options = {
 				'strokeColor': api.getAs(String, 'color'),
+				'strokeOpacity': api.getAs(Number, 'opacity'),
+				'strokeWeight': api.getAs(Number, 'weight'),
 				'markerOptions': api.getAs(String, 'icon') ? { icon: { url: api.getAs(String, 'icon') } } : {}
 			};
 		var polyLine = animated === true ? map.createPolyLineAnimated(options) : map.createPolyLine(options);
